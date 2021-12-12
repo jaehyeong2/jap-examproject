c694634605f57da3bb33f0c09a1a98869c73cbb8
jae <wogud1514@naver.com>
2021-12-12 20:54:15 +0900

    order 내부에 로직 메서드 추가

--- src/main/java/com/jap/jpabook/domain/item/Item.java
+++ src/main/java/com/jap/jpabook/domain/item/Item.java
@@ -26,12 +26,12 @@ public class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
-    //business logic
+    //비즈니스 로직
     public void addStock(int quantity){
         this.stockQuantity += quantity;
     }
 
-    public void removeStorck(int quantity){
+    public void removeStock(int quantity){
         int restStock = this.stockQuantity - quantity;
         if(restStock <0 ){
             throw new NotEnoughStockException("need more stock");
